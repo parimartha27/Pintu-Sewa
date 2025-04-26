2b42c7383858ff978bf72cdd3057f73f2ae45f0f
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-04-26 16:40:27 +0700

    Fix: server side into client side

--- app/input-confirmation/page.tsx
+++ app/input-confirmation/page.tsx
@@ -1,15 +1,17 @@
-import InputConfirmationContentLayout from "@/components/layout/inputInformation/inputConfirmation/InputConfirmationContent";
-import InputInformationForm from "@/components/layout/inputInformation/InputInformationForm";
-import LeftCircleBackgroundLayout from "@/components/layout/LeftCircleBackground";
+"use client"
+
+import InputConfirmationContentLayout from "@/components/layout/inputInformation/inputConfirmation/InputConfirmationContent"
+import InputInformationForm from "@/components/layout/inputInformation/InputInformationForm"
+import LeftCircleBackgroundLayout from "@/components/layout/LeftCircleBackground"
 
 const InputConfirmationPage = () => {
   return (
     <LeftCircleBackgroundLayout>
       <InputInformationForm step={3}>
-        <InputConfirmationContentLayout/>
+        <InputConfirmationContentLayout />
       </InputInformationForm>
     </LeftCircleBackgroundLayout>
-  );
-};
+  )
+}
 
-export default InputConfirmationPage;
+export default InputConfirmationPage
