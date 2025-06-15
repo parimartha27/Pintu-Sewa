891933a00fc8a1ce3ba217eca858dab5e11d155e
PUTU AGUS PARIMARTHA <putu.parimartha@binus.ac.id>
2025-06-15 22:31:02 +0700

    Fix: remove route to dev page

--- components/layout/payment/SuccessPaymentModals.tsx
+++ components/layout/payment/SuccessPaymentModals.tsx
@@ -1,35 +1,34 @@
-"use client";
-import { Card, CardContent } from "@/components/ui/card";
-import Image from "next/image";
-import Success from "@/public/success.svg";
-import { useRouter } from "next/navigation";
+"use client"
+import { Card, CardContent } from "@/components/ui/card"
+import Image from "next/image"
+import Success from "@/public/success.svg"
+import { useRouter } from "next/navigation"
 
 const SuccessPaymentModals = () => {
-  const router = useRouter();
+  const router = useRouter()
   return (
-  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 px-4">
-  <Card className="w-full max-w-[400px] sm:max-w-[480px] lg:max-w-[540px] h-auto p-4 shadow-xl rounded-2xl bg-white pt-10">
-    <CardContent className="flex flex-col items-center justify-between h-full gap-4">
-      <h3 className="text-base sm:text-xl lg:text-2xl font-bold text-color-primaryDark text-center">
-        SELAMAT PEMBAYARAN BERHASIL
-      </h3>
-      <Image
-        width={198}
-        height={198}
-        src={Success}
-        alt="success"
-        className="w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] lg:w-[150px] lg:h-[150px]"
-      />
-      <p
-        onClick={() => router.push("/dev/end-flow")}
-        className="text-sm text-color-primaryDark hover:opacity-70 hover:cursor-pointer"
-      >
-        Selanjutnya
-      </p>
-    </CardContent>
-  </Card>
-</div>
-  );
-};
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 px-4'>
+      <Card className='w-full max-w-[400px] sm:max-w-[480px] lg:max-w-[540px] h-auto p-4 shadow-xl rounded-2xl bg-white pt-10'>
+        <CardContent className='flex flex-col items-center justify-between h-full gap-4'>
+          <h3 className='text-base sm:text-xl lg:text-2xl font-bold text-color-primaryDark text-center'>SELAMAT PEMBAYARAN BERHASIL</h3>
+          <Image
+            width={198}
+            height={198}
+            src={Success}
+            alt='success'
+            className='w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] lg:w-[150px] lg:h-[150px]'
+          />
+          <p
+            // onClick={() => router.push("/dev/end-flow")}
+            onClick={() => router.push("/")}
+            className='text-sm text-color-primaryDark hover:opacity-70 hover:cursor-pointer'
+          >
+            Selanjutnya
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
 
-export default SuccessPaymentModals;
+export default SuccessPaymentModals
